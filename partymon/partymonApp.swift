af8b88fb3f5e05abccf4c8d64aae210a8a241254
tylerj <tylerjcagle@gmail.com>
2022-03-11 15:45:32 -0700

    Initial Commit

---
+++ partymon/partymonApp.swift
@@ -0,0 +1,17 @@
+//
+//  partymonApp.swift
+//  partymon
+//
+//  Created by Tyler Cagle on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct partymonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
