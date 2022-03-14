41fb52995d11388a4b8d574ec3fb797171be201f
tylerj <tylerjcagle@gmail.com>
2022-03-13 20:07:11 -0600

    second commit

--- partymon/Utilities/Share.swift
+++ partymon/Utilities/Share.swift
@@ -5,4 +5,13 @@
 //  Created by Tyler Cagle on 3/11/22.
 //
 
-import Foundation
+import SwiftUI
+
+func presentShareSheet() {
+    guard let urlShare = URL(string: "https://tylerj.me") else { return }
+    let activityVC = UIActivityViewController(activityItems: [urlShare], applicationActivities: nil)
+    let scenes =  UIApplication.shared.connectedScenes
+    let windowScene = scenes.first as? UIWindowScene
+    let window = windowScene?.windows.first
+    window?.rootViewController?.present(activityVC, animated: true, completion: nil)
+}
